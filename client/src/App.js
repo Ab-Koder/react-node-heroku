43dcd89861a4dd9bc98e085c6f449b5cde78bd16
Abdurrahman <yabdurrahman478@gmail.com>
2020-09-02 11:07:48 +0530

    third commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
 import {BrowserRouter as Router, Route} from "react-router-dom"
-import "bootstrap/dist/css/bootstrap.min.css"
+
 
 import Navbar from "./components/navbar.component";
 import ExercisesList from "./components/exercises-list.component";
